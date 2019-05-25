2c35729b8191264bc7ec544f0b9bec5171184280
shrikanthkr <shrikanth_kr@yahoo.com>
2019-05-25 11:49:36 -0700

    use cookie for auth

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,22 +1,25 @@
 
 const jwt = require('jsonwebtoken');
 const config = require('../config.json');
+var cookie = require('cookie')
 const { secret } = config;
 function auth(client, next) {
     // invalid token - synchronous
     console.log("Auth");
     try {
-        let token = decodeHeader(client.handshake.query);
+        let token = decodeCookie(client.handshake.headers.cookie)
         decodedToken = jwt.verify(token, secret);
         return next();
     } catch (err) {
-        
-        return next(new Error({message: 'Invalid Token'}));
+        console.log(err);
+        return next(new Error({message: err}));
     }
 }
-function decodeHeader(headers){
-    if(headers.token != undefined) {
-        return headers.token
+function decodeCookie(cookieString){
+    let cookies = cookie.parse(cookieString)
+    if(cookies['ember_simple_auth-session'] != undefined) {
+      let authData = JSON.parse(cookies['ember_simple_auth-session']);
+      return authData.authenticated.token;
     }else{
         throw 'Missing token'
     }
