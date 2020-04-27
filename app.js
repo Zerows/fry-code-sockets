4a66edaacc07feb41925a1d7ed88f6b015c72b75
shrikanthkr <shrikanth_kr@yahoo.com>
2020-04-27 15:26:50 +0530

    add the socket flow to show user names

--- app.js
+++ app.js
@@ -7,15 +7,34 @@ const port = process.env.PORT || 4000
 io.use(auth);
 io.on('connection', client => {
   console.log("On Connect");
-  let room = client.handshake.query.room
+  let query = client.handshake.query
+  let room = query.room
   client.join(room)
   client.on('event', data => {
     client.broadcast.to(room).emit('event', data);
   });
+  resetConnectedUsers(io, room)
+
   client.on('disconnect', (e) => {
+    resetConnectedUsers(io, room)
     console.log(e);
   });
 });
+
+function resetConnectedUsers(io, room) {
+  let users = [];
+  io.of('/').in(room).clients((err, clients) => {
+    clients.map(function (clientId) {
+      let client = io.sockets.connected[clientId]
+      if (client != null) {
+        let user = client.handshake.query.user
+        users.push(user)
+      }
+    });
+    io.of('/').in(room).emit('users', users);
+  });
+}
+
 server.listen(port, () => {
   console.log("Listening on " + port);
 });
